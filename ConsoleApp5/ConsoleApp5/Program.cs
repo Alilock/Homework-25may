e698313cbcb482657ee67e769b4c875d0e066c1b
Cahanghir1 <jahanghiresh@code.edu.az>
2022-05-26 10:41:23 +0400

    Tasks

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -6,15 +6,43 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            int a = int.Parse(Console.ReadLine());
-            Console.WriteLine(Faktorial(a));
 
-            
 
-            
+            //-Layihe ise dusdukde sizden adlarin sayini istesin. Daha sonra o say qeder adlari daxil edin.
+            // Daha sonra axtardiginiz adi istesin, onu da daxil etdikden sonra siyahida olub olmadigini qaytarsin
+
+            Console.Write("Adlarin sayini daxil edin: ");
+            int count = int.Parse(Console.ReadLine());
+
+
+            string[] nameList = new string[count];
+
+            for (int i = 0; i < nameList.Length; i++)
+            {
+                Console.Write($"{i + 1}. Adi daxil edin: ");
+
+                nameList[i] = Console.ReadLine();
+            }
+
+            Console.Write("Axtardiginiz adi daxil edin: ");
+            bool check = false;
+            string wantedName = Console.ReadLine();
+
+            for (int j = 0; j < nameList.Length; j++)
+            {
+                if (nameList[j] == wantedName)
+                {
+                    check = true;
+
+                    break;
+                }
+            }
+            Console.WriteLine(check);
 
         }
 
+
+        #region dersde yazdiglarimiz
         static int isOdd(int[] numbers)
         {
             int count = 0;
@@ -83,21 +111,83 @@ namespace ConsoleApp5
             }
             return check;
         }
+        #endregion
 
-        static int Faktorial(int n)
+
+
+        //Verilmis adlar siyahisinda(string array) nece adin uzunlugunun 5-den boyuk oldugunu tapan proqram
+
+        static int CountOf5(string[] names)
         {
-            int fakt = 1;
-            for (int i = 1; i <= n; i++)
+            int count = 0;
+            for (int i = 0; i < names.Length; i++)
             {
-                fakt = fakt * i;
+                if (names[i].Length > 5)
+                {
+                    count++;
+                }
+
             }
-            return fakt;
+            return count;
         }
+
+        // Verilmis ededler siyahisindaki cut ededlerden ibraret yeni bir array qaytaran method.
+        // Misalcun gonderilmis ededler siyahisinda {1,3,4,5,6} deyerleri varsa method geriye {4,6} deyerlerinden ibaret bir array qaytarmalidir
+        //yaza bilmedim
+        static void OddFinder(int[] numbers)
+        {
+            int[] nums = { 2, 3, 4, 6 };
+            int count = 0;
+            int[] oddNums = new int[count];
+            int a = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+
+
+
+                if (nums[i] % 2 == 0)
+                {
+                    count++;
+                    oddNums = new int[count];
+
+                    a++;
+
+                    oddnums[a] = nums[i];
+
+                }
+
+
+
+
+
+
+
+            }
+
+            foreach (var num in oddNums)
+            {
+                Console.WriteLine(num);
+            }
+
+
+
+
+
+        }
+
     }
-    
 
 
 
 }
+
+
+
+
+
+
+
+
 
 
